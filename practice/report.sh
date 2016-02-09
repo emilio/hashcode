9559a8101dbcd44d77998262223825ddb0df6f5d
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-02-09 15:56:58 +0100

    Add more minimal inputs to test certain small cases

--- practice/report.sh
+++ practice/report.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 TO_TRY="practice.py practice"
+INPUTS="logo right_angle learn_and_teach"
 
-for i in *.in; do
-  for program in $TO_TRY; do
-    ./$program $i > "$program.$i.out" 2> /dev/null
-    echo "$program $i --> $(head -1 "$program.$i.out")"
+for program in $TO_TRY; do
+  echo "# $program"
+  for i in $INPUTS; do
+    ./$program inputs/$i.in > "outputs/$program.$i.out" 2> /dev/null
+    echo "$(head -1 "outputs/$program.$i.out")    $i"
   done
+  echo ""
 done
